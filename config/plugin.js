443101c82470816c8cc7c51083bdc4f454bc22c4
kelvyn <kelvyn2reis@gmail.com>
2022-07-06 11:41:37 -0300

    fix error 1

--- config/plugin.js
+++ config/plugin.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         cloud_name: env("CLOUDINARY_CLOUD_NAME"),
         api_key: env("CLOUDINARY_API_KEY"),
-        api_secret: env("CLOUDINARY_API_KEY"),
+        api_secret: env("CLOUDINARY_API_SECRET"),
       },
       actionOptions: {
         upload: {},
